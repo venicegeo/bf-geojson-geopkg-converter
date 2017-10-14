923223a73b5e6a1e61fe34e749952f997e955570
Jeff Yutzler <jyutzler@yahoo.com>
2017-10-13 21:59:31 -0400

    checkpoint

---
+++ src/test/java/org/venice/beachfront/services/ShapefileConverterImplTest.java
@@ -0,0 +1,30 @@
+package org.venice.beachfront.services;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
+
+import junit.framework.TestCase;
+
+public class ShapefileConverterImplTest extends TestCase {
+    private ShapefileConverter shapefileConverter;
+
+    @Override
+    public void setUp() throws IOException {
+        this.shapefileConverter = new ShapefileConverterImpl();
+    }
+
+    public void testData() throws URISyntaxException, IOException {
+//    	processFile("LC80020522017095LGN00.geojson");
+//    	processFile("taz10.geojson");
+//    	processFile("fc-points.geojson");
+    	processFile("shorelines-fc.geojson");
+    }
+    private void processFile(String inFileName) throws URISyntaxException, IOException{
+		java.net.URL url = ClassLoader.getSystemResource(inFileName);
+		java.nio.file.Path resPath = java.nio.file.Paths.get(url.toURI());
+        byte[] result = this.shapefileConverter.apply(java.nio.file.Files.readAllBytes(resPath));
+        Assert.assertNotNull(result);
+    }
+}
